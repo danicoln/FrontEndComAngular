466a06932938babe978e24edf5a39b6c017ea1a9
danicoln <lincolndeveloper@outlook.com>
2023-03-30 16:22:56 -0300

    Adicionando estilos CSS em componentes

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -3,7 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-funcionario-card',
   templateUrl: './funcionario-card.component.html',
-  styleUrls: ['./funcionario-card.component.css']
+  //styleUrls: ['./funcionario-card.component.css']
+  //adicionando estilos css em componentes
+  styles: [`
+    .card-block{
+      text-transform: uppercase;
+      color: blue;
+    }
+
+  `]
 })
 export class FuncionarioCardComponent  {
 
